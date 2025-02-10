edde88b32ff198151a5a67af0aa22b0a3aec008f
rkemmey <rachel.kemmey@gmail.com>
2025-02-09 18:32:19 -0600

    updates

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,19 @@
 # How can you make this more scalable and reusable later?
+# an N-digit number that is equal to the sum of the Nth powers of its digits
+''' convert to string for length. take power of each and sum.
+test if equals, then add to list
+'''
+import math
+
 def find_armstrong_numbers(numbers):
+    arm_list = []
+
+    for n in numbers:
+        new_sum = 0
+        str_n = str(n)
+        power = len(str_n)
+        for s in str_n:
+            new_sum += math.pow(int(s), power)
+        if new_sum == n:
+            arm_list.append(n)
+    return arm_list
